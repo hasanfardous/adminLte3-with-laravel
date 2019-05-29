a5db38ea0aaacf43d5e98297008b818e9f379871
hasanfardous <frworkbd@gmail.com>
2019-05-29 23:09:28 +0600

    Printing service added

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
+Route::get('invoice', function(){
+    return view('invoice');
+});
 Route::get('{path}', "Homecontroller@index")->where( 'path', '([A-z\d\/_.]+)?' );
