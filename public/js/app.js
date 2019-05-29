c37a7413d08dfa9b83b07ff414b45002371447ea
hasanfardous <frworkbd@gmail.com>
2019-05-29 11:47:12 +0600

    Frontend viewed by user roles

--- public/js/app.js
+++ public/js/app.js
@@ -2335,7 +2335,7 @@ __webpack_require__.r(__webpack_exports__);
     loadUsers: function loadUsers() {
       var _this4 = this;
 
-      if (this.$gate.isAdmin()) {
+      if (this.$gate.isAdminOrAuthor()) {
         axios.get('api/user').then(function (_ref) {
           var data = _ref.data;
           return _this4.users = data.data;
@@ -62892,7 +62892,7 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", { staticClass: "container" }, [
-    _vm.$gate.isAdmin()
+    _vm.$gate.isAdminOrAuthor()
       ? _c("div", { staticClass: "row mt-5" }, [
           _c("div", { staticClass: "col-12" }, [
             _c("div", { staticClass: "card" }, [
@@ -62978,7 +62978,7 @@ var render = function() {
         ])
       : _vm._e(),
     _vm._v(" "),
-    !_vm.$gate.isAdmin() ? _c("div", [_c("not-found")], 1) : _vm._e(),
+    !_vm.$gate.isAdminOrAuthor() ? _c("div", [_c("not-found")], 1) : _vm._e(),
     _vm._v(" "),
     _c(
       "form",
@@ -79305,6 +79305,20 @@ function () {
     key: "isUser",
     value: function isUser() {
       return this.user.type === 'user';
+    }
+  }, {
+    key: "isAdminOrAuthor",
+    value: function isAdminOrAuthor() {
+      if (this.user.type === 'admin' || this.user.type === 'author') {
+        return true;
+      }
+    }
+  }, {
+    key: "isAuthorOrUser",
+    value: function isAuthorOrUser() {
+      if (this.user.type === 'author' || this.user.type === 'user') {
+        return true;
+      }
     }
   }]);
 
--- resources/js/Gate.js
+++ resources/js/Gate.js
@@ -13,6 +13,18 @@ export default class Gate{
         return this.user.type === 'user';
     }
 
+    isAdminOrAuthor(){
+        if( this.user.type === 'admin' || this.user.type === 'author' ){
+            return true;
+        }
+    }
+
+    isAuthorOrUser(){
+        if( this.user.type === 'author' || this.user.type === 'user' ){
+            return true;
+        }
+    }
+
 
 
 }
